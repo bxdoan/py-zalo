d95d7cc3ff9c75ffc563028ebc085321660caf15
Doan Bui <bxdoan93@gmail.com>
2023-01-05 15:34:09 +0700

    config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = 'py-zalo',
+    packages = find_packages(),
+    version = '1.0.0',
+    description = 'Pretty simple and fully asynchronous framework for working with Zalo API',
+    author = 'Doan bui',
+    author_email = 'hi@bxdoan.com',
+    url = 'https://github.com/bxdoan/py-zalo',
+    keywords = [],
+    classifiers = []
+)
